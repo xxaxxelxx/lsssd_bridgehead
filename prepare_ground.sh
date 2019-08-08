d72936f6baed9f80032cdcb23862a24c6ada556d
xxaxxelxx <x@axxel.net>
2019-08-08 11:09:04 +0200

    Updated 2019-08-08 11:09

---
+++ prepare_ground.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# tools
+apt-get install -y rsync rdate mc mlocate
+
+# docker
+apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get -y install docker-ce
+
+# AXXEL.NET
+# 2019MAY21
+
+exit 0

001837ccb90d9b79c499a4a92d792373e67e3b52
xxaxxelxx <x@axxel.net>
2019-08-08 14:21:50 +0200

    Updated 2019-08-08 14:21

--- mysql_test_status.sh
+++ mysql_test_status.sh
@@ -9,17 +9,21 @@ echo "##########################################################################
 echo "All:"
 echo "SET @@time_zone = '$TZ';select mntpnt,status,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status;" | mysql -u root -p$(cat $MASTERSERVER_MYSQL_SECRET_ROOT_FILE) -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
 echo
+echo
 echo "_______________________________________________________________________________________________________________________________________________"
 echo "Outdated and unguarded:"
 echo "SET @@time_zone = '$TZ';select mntpnt,FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive < UNIX_TIMESTAMP() - ${ALIVE_LIMIT};" | mysql -u root -p$(cat $MASTERSERVER_MYSQL_SECRET_ROOT_FILE) -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+echo
 echo
 echo "_______________________________________________________________________________________________________________________________________________"
 echo "Proper guarded:"
 echo "SET @@time_zone = '$TZ';select mntpnt,status,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive >= UNIX_TIMESTAMP() - ${ALIVE_LIMIT};" | mysql -u root -p$(cat $MASTERSERVER_MYSQL_SECRET_ROOT_FILE) -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
 echo
+echo
 echo "_______________________________________________________________________________________________________________________________________________"
 echo "Sounding:"
 echo "SET @@time_zone = '$TZ';select mntpnt,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive >= UNIX_TIMESTAMP() - ${ALIVE_LIMIT} and status = 0;" | mysql -u root -p$(cat $MASTERSERVER_MYSQL_SECRET_ROOT_FILE) -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+echo
 echo
 echo "_______________________________________________________________________________________________________________________________________________"
 echo "Silent:"

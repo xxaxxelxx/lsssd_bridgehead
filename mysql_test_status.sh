0b5243e3f8c3b1b27438bf911ae222c930ae7f7d
xxaxxelxx <x@axxel.net>
2019-08-13 14:49:28 +0200

    Updated 2019-08-13 14:49

--- mysql_test_status.sh
+++ mysql_test_status.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
-
+echo "A  | ALL"
+echo "U  | UNGUARDED"
+echo "G  | GUARDED"
+echo "SO | SOUNDING"
+echo "SI | SILENT"
 
 test -r CONFIG_MASTER || exit
 . CONFIG_MASTER
 MYSQLPWD="$(cat $MASTERSERVER_MYSQL_SECRET_ROOT_FILE | head -n 1 | tr -d '\n')"
 
+echo "###############################################################################################################################################"
+
+echo "$1" | grep -i '^A' > /dev/null && (
+echo "All:"
+echo "SET @@time_zone = '$TZ';select mntpnt,status,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status;" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+)
+
+echo "$1" | grep -i '^U' > /dev/null && (
+echo "Outdated and unguarded:"
+echo "SET @@time_zone = '$TZ';select mntpnt,FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive < UNIX_TIMESTAMP() - ${ALIVE_LIMIT};" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+)
+
+echo "$1" | grep -i '^G' > /dev/null && (
+echo "Proper guarded:"
+echo "SET @@time_zone = '$TZ';select mntpnt,status,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive >= UNIX_TIMESTAMP() - ${ALIVE_LIMIT};" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+)
+
+echo "$1" | grep -i '^SO' > /dev/null && (
+echo "Sounding:"
+echo "SET @@time_zone = '$TZ';select mntpnt,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive >= UNIX_TIMESTAMP() - ${ALIVE_LIMIT} and status = 0;" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+)
+
+echo "$1" | grep -i '^SI' > /dev/null && (
+echo "Silent:"
+echo "SET @@time_zone = '$TZ';select mntpnt,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive >= UNIX_TIMESTAMP() - ${ALIVE_LIMIT} and status != 0;" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+)
+
+test "x$1" == "x" && (
 echo "###############################################################################################################################################"
 echo "All:"
 echo "SET @@time_zone = '$TZ';select mntpnt,status,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status;" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
@@ -28,5 +60,6 @@ echo
 echo "_______________________________________________________________________________________________________________________________________________"
 echo "Silent:"
 echo "SET @@time_zone = '$TZ';select mntpnt,FROM_UNIXTIME(since,\"%Y-%m-%d_%H:%i:%S\"),FROM_UNIXTIME(alive,\"%Y-%m-%d_%H:%i:%S\") from status where alive >= UNIX_TIMESTAMP() - ${ALIVE_LIMIT} and status != 0;" | mysql -u root -p$MYSQLPWD -P $MASTERSERVER_MYSQL_PORT -D silenceDB -h $MASTERSERVER_IP | column -t
+)
 
 exit $?
